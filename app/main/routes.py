95585f3cc6703100a2e3b40421814a7f7af24be1
Meribe Henry <meribehenry09@gmail.com>
2026-05-14 00:03:08 +0100

    Created a package called main and added routes.py to it. The routes.py contains the main blueprint

---
+++ app/main/routes.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, render_template
+from flask_login import login_required
+
+main = Blueprint("main", __name__)
+
+
+@main.route("/")
+@main.route("/home")
+def home():
+    return render_template("main/home.html")
+
+@main.route("/about")
+def about():
+    return render_template("main/about.html")
+
+@main.route("/dashboard")
+@login_required
+def dashboard():
+    return render_template("main/dashboard.html")
+
+@main.route("/leaderboard")
+@login_required
+def leaderboard():
+    return render_template("main/leaderboard.html")
