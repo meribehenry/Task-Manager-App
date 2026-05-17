e3d95e324938ed3176d0f159e0fcbc93ec7d65a8
Meribe Henry <meribehenry09@gmail.com>
2026-05-17 10:57:48 +0100

    Added flask-mail to requirements.txt and added the necessary configuration to config.py

--- config.py
+++ config.py
@@ -5,4 +5,9 @@ class Config:
     SECRET_KEY=os.environ.get("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URL", "sqlite:///app.db")
     UPLOAD_FOLDER=os.environ.get("UPLOAD_FOLDER")
-    MAX_CONTENT_LENGT=int(os.environ.get("MAX_CONTENT_LENGTH", (3 * 1024 * 1024)))
+    MAX_CONTENT_LENGTH=int(os.environ.get("MAX_CONTENT_LENGTH", (3 * 1024 * 1024)))
+    MAIL_SERVER =os.environ.get("MAIL_SERVER")
+    MAIL_PORT=int(os.environ.get("MAIL_PORT", 587))
+    MAIL_USE_TLS=os.environ.get("MAIL_USE_TLS", "False").lower() in ["true", "1", "t"]
+    MAIL_USERNAME=os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD=os.environ.get("MAIL_PASSWORD")
