a17dbb0af601458dcabe9f9f2ad551609be026af
Meribe Henry <meribehenry09@gmail.com>
2026-05-17 11:05:15 +0100

    Added the reset password routes to app/auth/routes.py and add the send_reset_email function to app/auth/utilis.py

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,11 +1,10 @@
-from datetime import datetime
-
-from .utilis import load_user
+from .utilis import load_user, send_reset_email
 from flask import Blueprint, render_template, redirect, flash, url_for
-from flask_login import login_user, logout_user, logout_user
-from app.forms import RegistrationForm, LoginForm
+from flask_login import current_user, login_user, logout_user, logout_user
+from app.forms import RegistrationForm, LoginForm, ResetPasswordForm, ResetRequestForm
 from app.models import User
 from app.extensions import bcrypt, db
+
 
 auth = Blueprint("auth", __name__)
 
@@ -53,3 +52,35 @@ def logout():
     logout_user()
     flash("Your have logged out", "success")
     return redirect(url_for("main.home"))
+
+
+@auth.route("/reset_password", methods=["GET", "POST"])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect("main.dashboard")
+    
+    form = ResetRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        send_reset_email(user)
+        flash("A message was sent to your email with the reset password link", "success")
+        return redirect(url_for("auth.login"))
+    return render_template("auth/reset_request.html", form=form, title="Reset Password")
+
+
+@auth.route("/reset_password<token>", methods=["GET", "POST"])
+def reset_password(token):
+    if current_user.is_authenticated:
+        return redirect("main.dashboard")
+    
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user = User.verify_reset_token(token)
+        if not user:
+            flash("Reset token is invalid or has expired", "danger")
+            return redirect(url_for("auth.reset_request"))
+        user.password = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+        db.session.commit()
+        flash("Password successfully changed", "success")
+        return redirect(url_for("auth.login"))
+    return render_template("auth/reset_password.html", form=form, title="Reset Password")
--- app/auth/utilis.py
+++ app/auth/utilis.py
@@ -1,6 +1,20 @@
+from flask import url_for
 from app.extensions import login_manager
 from app.models import User
+from app.extensions import mail
+from flask_mail import Message
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    message = Message("Password Reset Request", sender="noreply@gmail.com", recipients=[user.email])
+    message.body = f"""
+Please click the link below to reset your password
+Link: {url_for("auth.reset_password", token=token, _external=True)}
+
+If you didn't request this, just ignore the message nothing would be changed.
+"""
+    mail.send(message)
