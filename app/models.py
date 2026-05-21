e59fb261a32729bc93f243ce2f2bc62240e60564
Meribe Henry <meribehenry09@gmail.com>
2026-05-21 13:04:33 +0100

    Changed the profile_pic attribute to profile_pic_url and also added profile_pic_id in the User model. Plus added extra layer of security to the verify_reset_token method by passing in the inital password hash to the token and and verifing if it the same before returning User object.

--- app/models.py
+++ app/models.py
@@ -9,9 +9,13 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     gender = db.Column(db.String(10), nullable=False)
-    profile_pic = db.Column(db.String(20), default="default_pic.jpg", nullable=False)
+    profile_pic_id = db.Column(db.String(100), default="default", nullable=False)
+    profile_pic_url = db.Column(
+                                db.String(255), 
+                                default="https://res.cloudinary.com/ddzmfmexs/image/upload/v1779357666/default_bqkr3f.jpg", 
+                                nullable=False)
     completed_task = db.Column(db.Integer, default=0, nullable=False)
     tasks = db.relationship("Task", backref="user", lazy=True)
 
@@ -24,22 +28,29 @@ class User(db.Model, UserMixin):
     
     def get_reset_token(self):
         s = URLSafeTimedSerializer(current_app.config["SECRET_KEY"])
-        return s.dumps({"user_id": self.id})
+        return s.dumps({"user_id": self.id, "password_hash": self.password})
     
     @staticmethod
     def verify_reset_token(token, max_age=1800):
         s = URLSafeTimedSerializer(current_app.config["SECRET_KEY"])
         try:
-            user_id = s.loads(token, max_age=max_age)["user_id"]
-            return User.query.get(user_id)
+            data = s.loads(token, max_age=max_age)["user_id"]
+            user_id = data["user_id"]
+            password_hash = data["password_hash"]
         
         except (BadSignature, SignatureExpired):
             return None
-
+        
+        user = User.query.get(user_id)
+        if user.password != password_hash:
+            return None
+        
+        return user
+        
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    task = db.Column(db.String(100), nullable=False)
+    task = db.Column(db.String(500), nullable=False)
     date_added = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     deadline = db.Column(db.DateTime, nullable=False)
     is_completed = db.Column(db.Boolean, default=False, nullable=False)
